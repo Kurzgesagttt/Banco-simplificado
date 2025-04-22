6c601feb4d72daf2136969a0460c9a9359897efa
Kurzgesagt <lucasmorenorodrigues@hotmail.com>
2025-04-21 21:02:41 -0300

    notification service

--- src/main/java/com/kurzgts/PicPay/services/NotificationService.java
+++ src/main/java/com/kurzgts/PicPay/services/NotificationService.java
@@ -1,11 +1,15 @@
 package com.kurzgts.PicPay.services;
 
+import com.kurzgts.PicPay.exceptions.OperacaoNaoPermitidaException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 public class NotificationService {
@@ -17,16 +21,28 @@ public class NotificationService {
 
     @Async(value = "emailSenderThread")
     public void enviarNotificacao(String destinatario, String assunto, String corpo) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setTo(destinatario);
-        mensagem.setSubject(assunto);
-        mensagem.setText(corpo);
-        mensagem.setFrom("lucaskappa66@gmail.com");
-        javaMailSender.send(mensagem);
+        if(authorizationRequestApi()){
+            SimpleMailMessage mensagem = new SimpleMailMessage();
+            mensagem.setTo(destinatario);
+            mensagem.setSubject(assunto);
+            mensagem.setText(corpo);
+            mensagem.setFrom("lucaskappa66@gmail.com");
+            javaMailSender.send(mensagem);
+        }else{
+            throw new OperacaoNaoPermitidaException("Serviço de notificação offline");
+        }
     }
 
     public boolean authorizationRequestApi() {
         String url = "https://util.devi.tools/api/v1/notify";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Void.class);
+
+        return response.getStatusCode() == HttpStatus.NO_CONTENT;
 
     }
 
