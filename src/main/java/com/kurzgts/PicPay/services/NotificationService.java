d85e9c843fc3927a90912d9b0e2234985ff10e39
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-05-23 14:08:49 -0300

    email services update

--- src/main/java/com/kurzgts/PicPay/services/NotificationService.java
+++ src/main/java/com/kurzgts/PicPay/services/NotificationService.java
@@ -13,30 +13,20 @@ import org.springframework.web.client.RestTemplate;
 public class NotificationService {
 
     @Autowired
-    JavaMailSender javaMailSender;
-    @Autowired
     RestTemplate restTemplate;
 
     @Async(value = "emailSenderThread")
     public void enviarNotificacao(String destinatario, String assunto, String corpo) {
-        if(authorizationRequestApi()){
-            SimpleMailMessage mensagem = new SimpleMailMessage();
-            mensagem.setTo(destinatario);
-            mensagem.setSubject(assunto);
-            mensagem.setText(corpo);
-            mensagem.setFrom("lucaskappa66@gmail.com");
-            javaMailSender.send(mensagem);
-        }else{
-            throw new OperacaoNaoPermitidaException("Serviço de notificação offline");
-        }
+
     }
 
-    public boolean authorizationRequestApi() {
-        String url = "https://util.devi.tools/api/v1/notify";
-
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-        ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Void.class);
-        return response.getStatusCode() == HttpStatus.NO_CONTENT;
-    }
+    //nao utilizado atualmente
+//    public boolean authorizationRequestApi() {
+//        String url = "https://util.devi.tools/api/v1/notify";
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+//        ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Void.class);
+//        return response.getStatusCode() == HttpStatus.NO_CONTENT;
+//    }
 }
